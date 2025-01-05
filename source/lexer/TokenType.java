f9f3fcc2fd9a9481f3f3642c65cd6757a05dfbd0
Clarence Caluag <145199840+Clangudoo@users.noreply.github.com>
2025-01-06 00:02:22 +0800

    Update TokenType.java

--- source/lexer/TokenType.java
+++ source/lexer/TokenType.java
@@ -1,13 +1,12 @@
 package source.lexer;
 
+
 /**
  * Defines the possible categories of tokens that the lexical analyzer can recognize. 
  * Examples include IDENTIFIER, KEYWORD, OPERATOR, LITERAL, and DELIMITER.
  * This enumeration provides structure for classifying tokens.
  */
 
-public class TokenType {
-    
 public enum TokenType {
 
      // IDENTIFIERS
