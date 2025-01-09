4b60685dbaac2a25951101d43b9253bd5c2c66d8
Clarence Caluag <145199840+Clangudoo@users.noreply.github.com>
2025-01-09 21:34:16 +0800

    Update SpecialWords.java

--- code/language/SpecialWords.java
+++ code/language/SpecialWords.java
@@ -13,7 +13,6 @@ public class SpecialWords {
      * List of reserved, key, and noise words
      */
     private final String[] reservedWords = {"abstract", "after", "ALIAS", "before", "bool", "byte", "char", "class", "Complex", "Date", "double", "exclude", "export_as", "Frac", "filter_by", "final", "float", "inline_query", "inspect", "int", "long", "main", "modify", "native", "private", "protected", "public", "Rational", "short", "static", "STRICT", "strictfp", "str", "today", "toMixed", "transient", "validate", "volatile", "isValid"};
-    private final String[] keywords = {"break", "case", "day", "default", "do", "else", "exit", "exit-when", "for", "get", "if", "in", "Input", "month", "Output", "print", "switch", "switch-fall", "while", "where-type", "year"};
     private final String[] keywords = {"break", "case", "day", "default", "do", "else", "exit", "exit-when", "for", "from", "get", "having", "if", "in", "Input", "limit", "month", "order_by", "Output", "print", "select", "switch", "switch-fall", "System", "while", "where", "where-type", "year"};
     private static final String noiseWord = "general";
 
